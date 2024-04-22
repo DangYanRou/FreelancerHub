7f84ccbc47ef64edb27eade27b600911c4c77fe4
Agnes <agnes5078@gmail.com>
2024-04-22 16:13:24 +0800

    freelancerProfile

---
+++ freelancerhub/src/components/RatingStyles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    `
+export const Radio = styled.input`
+    display: none;
+    `
+export const Rating = styled.div`
+    cursor: pointer;
+    `
