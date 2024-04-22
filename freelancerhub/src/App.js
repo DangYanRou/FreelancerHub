25370a46c6bbbb257bb3b06f96b494ce5d0767e9
yongjing479 <122367568+yongjing479@users.noreply.github.com>
2024-04-22 13:51:44 +0800

    Project completed page done

--- freelancerhub/src/App.js
+++ freelancerhub/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState }  from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import FreelancerLandingPage from './pages/Freelancer/FreelancerLandingPage';
 import ProposalForm from './pages/Freelancer/ProposalForm';
@@ -9,6 +9,10 @@ import ClientFeedbackPage from './pages/Clients/ClientFeedbackPage';
 import FreelancerFeedbackPage from './pages/Freelancer/FreelancerFeedbackPage';
 import FreelancerAverageReviewBox from './pages/Freelancer/FreelancerAverageReviewBox';
 import ClientAverageReviewBox from './pages/Clients/ClientAverageReviewBox';
+import ProjectPosted from './pages/Clients/ProjectPosted';
+import ProjectDetailsPage from './pages/Freelancer/ProjectDetailsPage';
+import ProjectCompletedPage from './pages/Freelancer/ProjectCompletedPage';
+
 
 function App() {
   return (
@@ -32,6 +36,8 @@ function FreelancerRoutes() {
       <Route path="proposal-form" element={<ProposalForm />} />
       <Route path="freelancer-feedback-page" element={<FreelancerFeedbackPage />} />
       <Route path="freelancer-average-review-box" element={<FreelancerAverageReviewBox />} />
+      <Route path="project-details" element={<ProjectDetailsPage />} />
+      <Route path="project-completed-page" element={<ProjectCompletedPage />} />
       {/* Add more nested routes as needed */}
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
@@ -39,14 +45,18 @@ function FreelancerRoutes() {
 }
 
 function ClientRoutes() {
+ 
   return (
+   
     <Routes>
       <Route path="/" element={<ClientLandingPage />} />
       <Route path="client-feedback-page" element={<ClientFeedbackPage />} />
       <Route path="client-average-review-box" element={<ClientAverageReviewBox />} />
+      <Route path="project-posted" element={<ProjectPosted />} />
       {/* Add more nested routes as needed */}
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
+  
   );
 }
 
--- freelancerhub/src/pages/Freelancer/ProjectsApplied.js
+++ freelancerhub/src/pages/Freelancer/ProjectsApplied.js
@@ -1,11 +1,140 @@
 import React from 'react'
+import '../../styles/Freelancers/ProjectsApplied.css'
 import NavigationBar from './NavigationBarFreelancer';
+import ProjectList from '../../components/ProjectList';
+import { useState } from 'react';
+import { GrFormClose } from "react-icons/gr";
+import ProjectDetailsPage from './ProjectDetailsPage';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import StatusBar from '../../components/statusBar';
+
+
+const ProjectDetails =({project}) => {
+
+  const [status, setStatus] = useState(0);
+  if(!project)return null;
+  return(
+    <div className="project-details">
+     
+      <Link to="/freelancers/project-details" className="link">{project.title}</Link><br></br>
+      <a href="#" className="hover-profileLink">{project.client}</a>
+          <p id="category">{project.category}</p>
+          <p>{project.location}</p>
+          <p>{project.budget}/project</p>
+          <h3 id="about-the-project">About the Project:</h3>
+          <p id="description">{project.description}</p>
+          <div>
+            <h3 id="key-requirement">Key Requirements:</h3>
+            <ul className="list">
+              {project.items.map((item,index)=>(
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <h3 id="status">Status:</h3>
+            <p className="detail-status">{project.status}</p>
+           <p className="detail-date">{project.applyDate}</p>
+           {/*  status bar*/}
+           < StatusBar status={status}/>
+          </div>
+          <div className='button-container'>
+          <button className="btn-primary">View Application</button>
+          <button className="btn-secondary">Cancel Application</button>
+          </div>
+
+
+    </div>
+  );
+};
+
+const ProjectModal = ({ isOpen, onClose, project }) => {
+  if (!isOpen||!project) return null;
+
+  return (
+      <div className="modal-overlay" onClick={onClose}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-header">
+              <h2>View Application</h2>
+            <button className="close-btn" onClick={onClose}><GrFormClose /></button>
+            </div>
+             <ProjectDetails project={project}/>{/* Pass project to ProjectDetails */}
+              
+          </div>
+      </div>
+  );
+}
+
 
 const ProjectsApplied = () => {
+
+  const [selectedProject,setSelectedProject]=useState(null);
+  // Initial status index
+ 
+
+ const projects=[
+   {
+     title: 'Online Shopping Website Design',
+   client: 'Hana Florist',
+   category: 'Information & Communication Technology',
+   budget: 'RM 1500-RM 2000',
+   location: 'Petaling Jaya, Selangor'
+   ,id: 1,
+   description: `We are a flourishing florist business seeking a skilled freelancer to help us design our online shopping website. As the owner of a thriving florist, we recognize the importance of establishing a strong online presence to cater to our customers' evolving needs. We aim to create a seamless and aesthetically pleasing online shopping platform that reflects the beauty and elegance of our floral arrangements.`,
+   items: [
+     'User-Friendly Design',
+     'Aesthetic Appeal',
+     'Mobile Responsiveness',
+     'Integration of E-Commerce Features'
+   ],status:'Applied on FreelancerHub',applyDate:'1 May 2024'
+   },{
+     title: 'Online Shopping Website Design',
+   client: 'Hana Florist',
+   category: 'Information & Communication Technology',
+   budget: 'RM 1500-RM 2000',
+   location: 'Petaling Jaya, Selangor',
+   id: 2,
+   description: `We are a flourishing florist business seeking a skilled freelancer to help us design our online shopping website. As the owner of a thriving florist, we recognize the importance of establishing a strong online presence to cater to our customers' evolving needs. We aim to create a seamless and aesthetically pleasing online shopping platform that reflects the beauty and elegance of our floral arrangements.`,
+   items: [
+     'User-Friendly Design',
+     'Aesthetic Appeal',
+     'Mobile Responsiveness',
+     'Integration of E-Commerce Features'
+   ]
+   },{
+     title: 'Accountant',
+   client: 'Mr Honey Bees Farm',
+   category: 'Accounting',
+   budget: 'RM 1500-RM 2000',
+   location: 'Petaling Jaya, Selangor',
+   id: 3,
+   description: `We are a flourishing florist business seeking a skilled freelancer to help us design our online shopping website. As the owner of a thriving florist, we recognize the importance of establishing a strong online presence to cater to our customers' evolving needs. We aim to create a seamless and aesthetically pleasing online shopping platform that reflects the beauty and elegance of our floral arrangements.`,
+   items: [
+     'User-Friendly Design',
+     'Aesthetic Appeal',
+     'Mobile Responsiveness',
+     'Integration of E-Commerce Features'
+   ]
+   }
+ ];
+
+ const handleProjectClick = (project) => {
+   setSelectedProject(project);
+}
+
+const handleCloseModal = () => {
+   setSelectedProject(null);
+}
+
+
   return (
-    <div>
+    <div className="ProjectsApplied">
       <NavigationBar></NavigationBar>
-      <h1>ProposalApplied</h1>
+      <h2 className="page-header">Projects Applied</h2>
+      <div className="centered-container">
+   <ProjectList projects={projects} onProjectClick={handleProjectClick}   selectedProjectId={selectedProject ? selectedProject.id : null}/>
+   </div>
+   <ProjectModal isOpen={selectedProject !== null} onClose={handleCloseModal} project={selectedProject} />
+   
+       
     </div>
   )
 }
