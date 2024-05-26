d1c09780b15c49e3a5362e7596bfaa72983fa764
Agnes <agnes5078@gmail.com>
2024-05-27 01:17:35 +0800

    login, register and resetpass page backend done

---
+++ freelancerhub/frontend/src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-use-history';
+import "./styles/LoginPage.css";
+import { FaUser, FaLock } from "react-icons/fa";
+import { Link, useNavigate} from "react-router-dom";
+import { useUser } from '../../context/UserContext';
+import Switch from "../../components/Switch";
+import { useNotification } from '../../context/NotificationContext';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {getDocs,collection, where, query} from 'firebase/firestore'
+import { db,auth } from '../../firebase';
+import { get } from 'vant/lib/utils';
+
+
+const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [collectionName, setCollectionName] = useState('freelancers');
+    const { updateUser } = useUser();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const history = useHistory();
+    const navigate = useNavigate();
+
+    const handleToggle = (newState) => {
+        console.log('Switch is now', newState ? 'FREELANCER' : 'CLIENT');
+        setCollectionName(newState ? 'freelancers' : 'clients');
+      };
+      
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        const dbref = collection(db, collectionName);
+        const matchUsername = query(dbref, where('username', '==', email));
+        const matchEmail = query(dbref, where('email', '==', email));
+        try {
+            let userCredential;
+            const usernameSnapshot = await getDocs(matchUsername);
+            const usernameArray = usernameSnapshot.docs.map((doc) => doc.data());
+            const emailSnapshot = await getDocs(matchEmail);
+            const emailArray = emailSnapshot.docs.map((doc) => doc.data());
+    
+            if (usernameArray.length > 0) {
+                const matchedUser = usernameArray[0];
+                userCredential = await signInWithEmailAndPassword(auth, matchedUser.email, pass);
+            } else if (emailArray.length > 0) {
+                const matchedUser = emailArray[0];
+                userCredential = await signInWithEmailAndPassword(auth, matchedUser.email, pass);
+            } else {
+                throw new Error('No user found.');
+            }
+    
+            console.log(userCredential, "authData");
+            setIsLoggedIn(true);
+            if (collectionName === 'freelancers') {
+                updateUser({id:"freelancerID1",type:"freelancer"});
+                navigate("/freelancers/explore");
+            } else if (collectionName === 'clients') {
+                updateUser({id:"clientID1",type:"client"});
+                navigate("/clients/post-project");
+            }
+        } catch (error) {
+            if (error.code === 'auth/user-not-found') {
+                alert('No user found.');
+            } else if (error.code === 'auth/wrong-password') {
+                alert('Incorrect password.');
+            } else {
+                console.error('Error logging in:', error);
+                alert(`Error logging in: ${error.message}`);
+            }
+        }
+    };
+
+    //const { setPathname } = useNotification();
+    //setPathname(null);
+    // updateUser(null);
+
+    // const handlefreelancersClick = () => {
+    //     setIsLoggedIn(true);
+    //     //setPathname('/freelancers/notifications');
+    //     history.push('/freelancers/explore');
+    // };
+
+    // const handleclientsClick = () => {
+    //     setIsLoggedIn(true);
+    //     //setPathname('/clients/notifications');
+    //     history.push('/clients/post-project');
+    // };
+
+        return(
+        <div className="loginPage">
+            <div className="wrapper">
+                <form onSubmit={handleSubmit}>
+                    <h1 style={{ color: 'white' }}>Login</h1>
+                    <div className="input-box" style={{ color: 'white' }}>
+                        <FaUser className="icon" /><input name="user" className="email" type="text" placeholder="Email/Username" onChange={(e) => setEmail(e.target.value)} required />
+                    </div>
+                    <div className="input-box" style={{ color: 'white' }}>
+                        <FaLock className="icon" /><input name="pass" className="pass" type="password" placeholder="Password" onChange={(e) => setPass(e.target.value)} required />
+                    </div>
+                    <div className="identity" style={{ color: 'white' }}>
+                        <label className="identity-label">
+                            <Switch
+                                id="role"
+                                className="switch"
+                                initialFreelancerState={true}
+                                onToggle={handleToggle}
+                                onColor='grey'
+                            />
+                        </label>
+                    </div>
+                    <div className="remember-forgot">
+                        <label style={{ color: 'white' }}><input type="checkbox" />Remember Me</label>
+                        <Link to="/resetPassword">Forgot password?</Link>
+                    </div>
+                    <button className='login-button' type="submit">Login</button>
+                    <div className="register-link">
+                        <p style={{ color: 'white' }}>Don't have an account? <Link to="/register">Register</Link></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+   );
+};
+
+export default LoginPage;
--- freelancerhub/src/pages/LoginPage/ResetPassword.js
+++ freelancerhub/src/pages/LoginPage/ResetPassword.js
@@ -2,8 +2,28 @@ import React from 'react';
 import { MdEmail, MdLockReset } from "react-icons/md";
 import { Link } from "react-router-dom";
 import "./ResetPassword.css";
+import { Link, useNavigate } from "react-router-dom";
+import "./styles/ResetPassword.css";
+import { useUser } from '../../context/UserContext';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../../firebase';
 
 const ResetPassword=()=>{
+    const history= useNavigate();
+
+    const handleSubmit= async(e)=>{
+        e.preventDefault()
+        const emailVal=e.target.elements.email.value;
+        try {
+            await sendPasswordResetEmail(auth, emailVal);
+            alert("Check your email for a password reset link");
+            history("/");
+        } catch (error) {
+            alert('Error sending password reset email:', error);
+            console.error(`Error: ${error.message} (${error.code})`);
+        }
+    }
+
     return(
         <div className="resetPage">
             <div className="wrapper">
@@ -13,6 +33,13 @@ const ResetPassword=()=>{
                         <p style={{ color:'white'}}>Enter your email and we'll send you a link to get back into your account.</p>
                         <div className="input-box">
                             <MdEmail className="emailIcon" style={{ color:'white'}}/><input type="text" placeholder="Email" required></input>
+                    <form onSubmit={handleSubmit} >
+                        <MdLockReset className="main_icon"/>
+                        <h1 style={{ color:'white'}}>Trouble Logging In?</h1>
+                        <p style={{ color:'white'}}>Enter your email and we'll send you a link to get back into your account.</p>
+                        <div className="input-box">
+                            <MdEmail className="emailIcon"/>
+                            <input name="email" type="email" placeholder="Email" required></input>
                         </div>
                         <button type="submit">Send</button>
                         <div className="register-link">
