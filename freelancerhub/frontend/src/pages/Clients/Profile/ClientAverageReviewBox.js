5bea88dc29e41a667097e3b997f31252993e5aa1
yongjing479 <122367568+yongjing479@users.noreply.github.com>
2024-05-29 19:23:51 +0800

    small changes on average review box

--- freelancerhub/frontend/src/pages/Clients/Profile/ClientAverageReviewBox.js
+++ freelancerhub/frontend/src/pages/Clients/Profile/ClientAverageReviewBox.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../../../styles/AverageReviewBox.css";
+import "../../../styles/Clients/ClientAverageReviewBox.css";
 
 function AverageReviewBox({ feedbacks }) {
   const [ratings, setRatings] = useState({ 5: 0, 4: 0, 3: 0, 2: 0, 1: 0 });
