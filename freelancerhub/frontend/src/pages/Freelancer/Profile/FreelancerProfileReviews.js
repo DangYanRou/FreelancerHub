3288328238da89eb015111de323e59e4221de7c1
yongjing479 <122367568+yongjing479@users.noreply.github.com>
2024-06-01 18:36:14 +0800

    feedback filtering based on time done

--- freelancerhub/frontend/src/pages/Freelancer/Profile/FreelancerProfileReviews.js
+++ freelancerhub/frontend/src/pages/Freelancer/Profile/FreelancerProfileReviews.js
@@ -10,12 +10,15 @@ import {
 } from "firebase/firestore";
 import { db } from "../../../firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { formatDistanceToNow } from "date-fns";
 
 const ClientProfileReviews = ({ feedbacks }) => {
   const [feedbacksWithUserNames, setFeedbacksWithUserNames] = useState([]);
 
   const fetchUserName = async (uid) => {
-    const userSnapshot = await getDocs(query(collection(db, "clients"), where("uid", "==", uid)));
+    const userSnapshot = await getDocs(
+      query(collection(db, "clients"), where("uid", "==", uid))
+    );
     if (!userSnapshot.empty) {
       return userSnapshot.docs[0].data().username;
     }
@@ -24,34 +27,39 @@ const ClientProfileReviews = ({ feedbacks }) => {
 
   useEffect(() => {
     const fetchUserNames = async () => {
-      const updatedFeedbacks = await Promise.all(feedbacks.map(async (feedback) => {
-        const name = await fetchUserName(feedback.from);
-        return { ...feedback, userName: name };
-      }));
+      const updatedFeedbacks = await Promise.all(
+        feedbacks.map(async (feedback) => {
+          const name = await fetchUserName(feedback.from);
+          return { ...feedback, userName: name };
+        })
+      );
       setFeedbacksWithUserNames(updatedFeedbacks);
     };
     fetchUserNames();
   }, [feedbacks]);
 
-
   return (
     <div className="reviews-container">
-      {feedbacksWithUserNames.map((feedback, index) => (
-        <div className="review-card" key={feedback.id}>
-          <div className="review-header">
-            <h3>{feedback.userName}</h3>
-            <span className="review-date">
-              {feedback.timestamp.toDate().toLocaleString("en-GB")}
-            </span>
-          </div>
-          <div className="review-body">
-            <div className="review-rating">
-              <p>Rating : {feedback.rating} / 5</p>
+      {feedbacksWithUserNames
+        .sort((a, b) => b.timestamp.toDate() - a.timestamp.toDate())
+        .map((feedback, index) => (
+          <div className="review-card" key={feedback.id}>
+            <div className="review-header">
+              <h3>{feedback.userName}</h3>
+              <span className="review-date">
+                {formatDistanceToNow(feedback.timestamp.toDate(), {
+                  addSuffix: true,
+                })}
+              </span>
             </div>
-            <p>{feedback.feedback}</p>
+            <div className="review-body">
+              <div className="review-rating">
+                <p>Rating : {feedback.rating} / 5</p>
+              </div>
+              <p>{feedback.feedback}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
