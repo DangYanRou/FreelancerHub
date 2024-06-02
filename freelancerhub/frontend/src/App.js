cb4d8cad0c633babd1eb00af7d1c27c66d4f30e7
Agnes <agnes5078@gmail.com>
2024-06-03 02:51:27 +0800

    restrict web authorization

--- freelancerhub/frontend/src/App.js
+++ freelancerhub/frontend/src/App.js
@@ -9,26 +9,55 @@ import { UserProvider, useUser } from './context/UserContext.js';
 import NotificationProvider from './context/NotificationContext.js';
 import WithNavigation from './nav/routes/WithNavigation';
 import './tailwind.css';
+import Layout from './components/Layout.js';
 
+const user_types={
+  public:"public",
+  freelancers:"freelancer",
+  clients:"client",
+}
 
-
+const current_user_type=user_types.public
 function App() {
   return (
     <div className="App">
       <UserProvider>
+        <Layout>
           <Routes>
-            <Route path="/" element={<LoginPage />} />
+            <Route path="/" element={<PublicElement><LoginPage /></PublicElement>} />
           </Routes>
-
-          <Routes> 
-            <Route path="/resetPassword" element={<ResetPassword />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/freelancers/*" element={<FreelancerRoutes />} />
-            <Route path="/clients/*" element={<ClientRoutes />} />
-          </Routes>
-      </UserProvider>
+            <Routes> 
+              <Route path="/resetPassword" element={<ResetPassword />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/freelancers/*" element={<FreelancerElement><FreelancerRoutes /></FreelancerElement>} />
+              <Route path="/clients/*" element={<ClientElement><ClientRoutes /></ClientElement>} />
+            </Routes>
+          </Layout>
+     </UserProvider>
     </div>
   );
+}
+
+function PublicElement({children}){
+  return <>{children}</>
+}
+
+function FreelancerElement({children}){
+  const { user } = useUser();
+  if (user.type===user_types.freelancers){
+    return <>{children}</>
+  }else{
+    return <Navigate to ={"/"}/>
+  }
+}
+
+function ClientElement({children}){
+  const { user } = useUser();
+  if (user.type===user_types.clients){
+    return <>{children}</>
+  }else{
+    return <Navigate to ={"/"}/>
+  }
 }
 
 export default App;
--- freelancerhub/frontend/src/nav/NavigationBarFreelancer.js
+++ freelancerhub/frontend/src/nav/NavigationBarFreelancer.js
@@ -6,6 +6,10 @@ import { UserProvider } from '../context/UserContext';
 const NavigationBarFreelancer = () => {
   const { pathname } = useLocation(); 
 
+  const handleLogout = async () => {
+    localStorage.removeItem("user");
+  };
+  
   return (
     
       <nav className="bg-white border-gray-200 dark:bg-gray-900 border-b">
@@ -44,7 +48,8 @@ const NavigationBarFreelancer = () => {
                 </Link>
               </li>
               <li>
-                <Link to="/" className={`block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}>
+                <Link to="/" className={`block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
+                onClick={handleLogout}>
                   Sign Out
                 </Link>
               </li>
